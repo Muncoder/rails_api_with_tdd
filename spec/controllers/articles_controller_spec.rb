da6e2daed1f3b8f7c992929ffe275176513001ee
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-11-04 16:55:23 +0000

    15.serializing articles

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -8,23 +8,17 @@ describe ArticlesController do
 		end
 
 		it 'should return proper json' do
-			create_list :article, 2
+			articles = create_list :article, 2
 			get :index
 			json = JSON.parse(response.body)
 			json_data = json['data']
-			expect(json_data.length).to eq(2)
-			expect(json_data[0]['attributes']).to eq({
-				"title" => "My awesome article 1",
-				"content" => "The content of my awesome article 1",
-				"slug" => "my-awesome-article-1"
-			})
-
-			expect(json_data[1]['attributes']).to eq({
-				"title" => "My awesome article 2",
-				"content" => "The content of my awesome article 2",
-				"slug" => "my-awesome-article-2"
-			})
-
+			articles.each_with_index do |article, index|
+				expect(json_data[index]['attributes']).to eq({
+					"title" => article.title,
+					"content" => article.content,
+					"slug" => article.slug
+				})
+			end
 		end
 	end
 end

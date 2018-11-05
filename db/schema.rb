e23126512bd3662bc91fdc92b66d1be42dda18f2
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-11-05 21:12:31 +0000

    24.Create user object

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181104115732) do
+ActiveRecord::Schema.define(version: 20181105204438) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.string "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "login", null: false
+    t.string "name"
+    t.string "url"
+    t.string "avatar_url"
+    t.string "provider"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end

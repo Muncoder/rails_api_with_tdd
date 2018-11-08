cdcb2440d010dc0ba475fe6cf3b4f0491abb4714
munirchittagong@yahoo.com <munirchittagong@yahoo.com>
2018-11-08 20:05:54 +0000

    27. Reuse existing users

--- spec/lib/user_authenticator_spec.rb
+++ spec/lib/user_authenticator_spec.rb
@@ -41,6 +41,16 @@ describe UserAuthenticator do
 					:user).and_return(user_data)
 			end
 
+			it 'should save the user when does not exists' do
+				expect{ subject  }.to change{ User.count }.by(1)
+				expect(User.last.name).to eq('John Smith')
+			end
+
+			it 'should reuse already registered user' do
+				user = create :user, user_data
+				expect{ subject }.not_to change{ User.count }
+				expect(authenticator.user).to eq(user)
+			end
 		end
 	end
 end
